4825fbef9e050efa786ba2d7ecb9acc27fc5688c
thegeekkid <brian@bscc.support>
2017-11-27 17:06:17 -0600

    Validation logic done

---
+++ Byte Dec-Bin converter/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Byte_Dec_Bin_converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.input = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.output = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(538, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a number (0-255):";
+            // 
+            // input
+            // 
+            this.input.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input.Location = new System.Drawing.Point(23, 72);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(528, 62);
+            this.input.TabIndex = 1;
+            this.input.TextChanged += new System.EventHandler(this.input_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 55);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Result:";
+            // 
+            // output
+            // 
+            this.output.AutoSize = true;
+            this.output.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.Location = new System.Drawing.Point(13, 214);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(37, 55);
+            this.output.TabIndex = 3;
+            this.output.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 323);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label output;
+    }
+}
+
---
+++ Byte Dec-Bin converter/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Byte_Dec_Bin_converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.input.KeyPress += new KeyPressEventHandler(input_KeyPress);
+        }
+
+        private void input_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            String AcceptedKeys = "0123456789";
+            if (!(AcceptedKeys.Contains(e.KeyChar.ToString())))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void input_TextChanged(object sender, EventArgs e)
+        {
+            int dec = int.Parse(input.Text);
+            if ((dec < 0) || (dec > 255))
+            {
+                MessageBox.Show("Invalid number - must be between 0 and 255.");
+                input.Text = input.Text.Substring(0, input.Text.Length - 1);
+                if (input.Text.Length > 0)
+                {
+                    input.SelectionStart = input.Text.Length;
+                    input.SelectionLength = 0;
+                }
+            }else
+            {
+
+            }
+        }
+    }
+}
